bf8a05242e7ac8303a4ba395772a84a894533452
unknown <201801083@daiict.ac.in>
2023-10-30 02:59:56 -0400

    Done

--- index.js
+++ index.js
@@ -24,6 +24,33 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+function isUnixTimestampString(value) {
+ 
+  if (/^\d+$/.test(value)) {
+      const unixTimestamp = parseInt(value, 10);
+      return unixTimestamp >= 0 && unixTimestamp <= 2147483648000;
+  }
+  return false;
+}
+
+// your first API endpoint... 
+app.get("/api/:value", function (req, res) {
+  let final_date;
+  if (isUnixTimestampString(req.params.value)) {
+     final_date = new Date(parseInt(req.params.value));
+  }
+  else {
+    final_date = new Date(req.params.value);
+  }
+   
+  let unix_timestamp = final_date.getTime();
+let date = final_date.toUTCString();
+
+
+  res.json({ "unix":unix_timestamp, "utc":date});
+  
+});
+
 
 
 // listen for requests :)
